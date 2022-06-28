2e078036a8a95a7ec58d458afe19b840f44ef86b
Roman Butsko <75183199+RomanButsko@users.noreply.github.com>
2022-06-28 16:32:26 +0300

    added slider

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__slider').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="carousel__left"><img src="img/slider-left.png"></button>',
+        nextArrow: '<button type="button" class="carousel__right"><img src="img/right-slider.png"></button>'
+    });
+});
