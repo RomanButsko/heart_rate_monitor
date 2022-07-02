f01ebe889563bd082b89bbff9b714bc5e0f34335
Roman Butsko <75183199+RomanButsko@users.noreply.github.com>
2022-07-02 17:30:10 +0300

    added tabs for product selection

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,23 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="carousel__left"><img src="img/slider-left.png"></button>',
         nextArrow: '<button type="button" class="carousel__right"><img src="img/right-slider.png"></button>'
     });
-});
+    $('ul.catalog_menu__items').on('click', 'li:not(.catalog_menu__item_active)', function() {
+        $(this)
+          .addClass('catalog_menu__item_active').siblings().removeClass('catalog_menu__item_active')
+          .closest('div.container').find('div.catalog_content').removeClass('catalog_content_active').eq($(this).index()).addClass('catalog_content_active');
+      });
+    function chooseCards(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog_cards_side__firstSide').eq(i).toggleClass('catalog_cards_side__firstSide_active');
+                $('.catalog_cards_side__secondSide').eq(i).toggleClass('catalog_cards_side__secondSide_active');
+        })
+    })
+}
+    chooseCards('.catalog_cards__link')
+    chooseCards('.catalog_cards_side__secondSide__link')
+})
+
+
+
