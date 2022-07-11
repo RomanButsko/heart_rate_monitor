c7c5b965b3ce3647f0a9ae831c21a6d2b103fe84
Roman Butsko <75183199+RomanButsko@users.noreply.github.com>
2022-07-11 21:43:09 +0300

    compiled files to /dist

---
+++ dist/js/script.js
@@ -0,0 +1,104 @@
+$(document).ready(function(){
+    $('.carousel__slider').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="carousel__left"><img src="img/slider-left.png"></button>',
+        nextArrow: '<button type="button" class="carousel__right"><img src="img/right-slider.png"></button>'
+    });
+    $('ul.catalog_menu__items').on('click', 'li:not(.catalog_menu__item_active)', function() {
+        $(this)
+          .addClass('catalog_menu__item_active').siblings().removeClass('catalog_menu__item_active')
+          .closest('div.container').find('div.catalog_content').removeClass('catalog_content_active').eq($(this).index()).addClass('catalog_content_active');
+      });
+    function chooseCards(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog_cards_side__firstSide').eq(i).toggleClass('catalog_cards_side__firstSide_active');
+                $('.catalog_cards_side__secondSide').eq(i).toggleClass('catalog_cards_side__secondSide_active');
+        })
+    })
+}
+    chooseCards('.catalog_cards__link')
+    chooseCards('.catalog_cards_side__secondSide__link')
+
+    //Modals
+
+    $('[data-modal=consultation]').on('click', () => {
+        $('.overheight, #consultation').fadeIn('slow')
+    })
+    $('.modal_close').on('click', () => {
+        $('.overheight,  #consultation').fadeOut('slow')
+    })
+    $('.button__card').on('click', () => {
+        $('.overheight, #order').fadeIn('slow')
+    })
+    $('.button__card').each(function(item) {
+        $(this).on('click', function() {
+            $('#order .modal_subdescr').text($('.catalog_cards__h3').eq(item).text())
+        })
+    })
+    $('.modal_close').on('click', () => {
+        $('.overheight,  #order').fadeOut('slow')
+    })
+
+    function changeForm(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                  },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            }, 
+            messages: {
+                name: {
+                        required: "Пожалуйста, введите ваше имя",
+                        minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста введите ваш телефон",
+                email: {
+                  required: "Пожалуйста, введите e-mail",
+                  email: "Почта введена неверна"
+                }
+        }
+    });
+    }
+
+    changeForm(".form_submit");
+    changeForm('#consultation form');
+    changeForm('#order form');
+
+    $('.form').submit(function(e) {
+        e.preventDefault();
+        $.ajax ({
+            type: 'POST',
+            url: '/src/mailer/smart.php',
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find('input').value = '';
+            $('#cosultation, #order').fadeOut()
+            $('.overheight', '#response').fadeIn()
+            $('form').trigger('reset');
+        })
+        return false
+    })
+
+    $(window).scroll(function() {
+        $(this).scrollTop() > 1600 ? $('.scrollPage').fadeIn() : $('.scrollPage').fadeOut()
+    })
+    $("a.scrollPage").click(function() {
+        const elementClick = $(this).attr("href")
+        const destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: destination
+        }, 800);
+        return false;
+        });
+})
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,16 @@ const rename = require("gulp-rename"); // editor doesn't allow to find this depe
 const sass = require("gulp-sass")(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
+    // gulp.watch('src/*.html').on('change', browserSync.reload)
 });
 
 
@@ -25,13 +28,35 @@ gulp.task('styles', function() {
                         cascade: false
                     }))
                 .pipe(cleanCSS({compatibility: 'ie8'}))
-                .pipe(gulp.dest("src/css"))
+                .pipe(gulp.dest("dist/css"))
                 .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'))
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'))
+    gulp.watch("src/*.html").on("change", gulp.parallel('html'));
 })
 
-gulp.task('default', gulp.parallel("watch", "server", "styles"));
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist/'));
+})
+
+
+gulp.task('scripts', function() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest('dist/js'));
+})
+
+gulp.task('fonts', function() {
+    return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'));
+})
+gulp.task('img', function() {
+    return gulp.src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+})
+
+gulp.task('default', gulp.parallel("watch", "server", "styles", "html", "scripts", "fonts", "img"));
